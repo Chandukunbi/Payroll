fc77d30be1e6152d69e72ce7863c3a51daacb180
Chandukunbi <chandukunbi@gmail.com>
2020-02-03 12:39:52 +0530

    Add project files.

---
+++ PayrollManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PayrollManagement.Startup))]
+namespace PayrollManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
